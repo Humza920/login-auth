8127c7f5fe7632471c09497dafbae1187d21d4f3
Humza920 <humza.rehman.779@gmail.com>
2024-07-30 11:37:59 -0700

    DONE

--- main.js
+++ main.js
@@ -1,103 +1,21 @@
-import {
-  signInWithEmailAndPassword,
-  GoogleAuthProvider,
-  signInWithPopup,
-  GithubAuthProvider,
-} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
-
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 
 import { auth } from "./config.js";
-
+const form = document.querySelector("#form");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
-const form = document.querySelector("#form");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  signInWithEmailAndPassword(auth, email.value, password.value)
+  createUserWithEmailAndPassword(auth, email.value, password.value)
     .then((userCredential) => {
-      const user = userCredential.user;
-      alert('You have sucessfully login !')
-      window.location="home.html"
+      console.log(userCredential);
+      alert("You have sucessfully Registered");
+      window.location = "index.html";
     })
     .catch((error) => {
       const errorMessage = error.message;
       console.log(errorMessage);
-      alert(`${errorMessage}`)
+      alert(`${errorMessage}`);
     });
 });
-
-
-
-
-//Login with google btn functionality
-
-const googleLogin = document.querySelector(".google-login-btn");
-
-const providerGoogle = new GoogleAuthProvider();
-
-googleLogin.addEventListener('click' , ()=>{
-
-  
-signInWithPopup(auth, providerGoogle)
-  .then((result) => {
-  
-    const user = result.user;
-    console.log(user);
-    window.location="home.html"
-    
-  })
-  .catch((error) => {
-    const errorMessage = error.message;
-    console.log(errorMessage);
-    // ...
-  });
-
-})
-
-
-
-
-//Login with github
-
-
-const GitHubLogin = document.querySelector(".github-login-btn");
-
-const providerGitHub = new GithubAuthProvider();
-
-GitHubLogin.addEventListener('click' , ()=>{
-
-  signInWithPopup(auth, providerGitHub)
-    .then((result) => {
-      const user = result.user;
-      console.log(user);
-      window.location='home.html'
-     
-    })
-    .catch((error) => {
-      const errorMessage = error.message;
-      console.log(errorMessage);
-      
-    });
-
-})
-
-
-//Email password functionallty
-
-document
-  .getElementById("toggle-password")
-  .addEventListener("click", function () {
-    const passwordField = document.getElementById("password");
-    const eyeIcon = document.querySelector("#toggle-password i");
-
-    if (passwordField.type === "password") {
-      passwordField.type = "text";
-      eyeIcon.classList.remove("fa-eye");
-      eyeIcon.classList.add("fa-eye-slash");
-    } else {
-      passwordField.type = "password";
-      eyeIcon.classList.remove("fa-eye-slash");
-      eyeIcon.classList.add("fa-eye");
-    }
-  });
--- page1.js
+++ page1.js
@@ -1,33 +1,140 @@
 import {
-    onAuthStateChanged,
-    signOut,
-  } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
-  
-  
-  import { auth } from "./config.js";
-  
-  const logOutBtn = document.querySelector("#log-out-btn");
-  
-  // const auth = getAuth();
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      const uid = user.uid;
-      console.log(uid);
-    } else {
-     window.location="login.html"
-    }
-  });
-  
-  
-  logOutBtn.addEventListener('click' , ()=>{
+  onAuthStateChanged,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+
+import {
+  collection,
+  addDoc,
+  getDocs,
+} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+
+import { auth, db } from "./config.js";
+
+//Firestore some cdn
+
+const logOutBtn = document.querySelector("#logoutBtn");
+
+//User display image
+
+const userImage = document.getElementById("user-image");
+const userName = document.getElementById("user-name");
+
+//profile
+
+const userProfile = document.querySelector("#modal-user-image");
+const userDisplayName = document.querySelector("#modal-user-name");
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    let userPhoto = user.photoURL;
+    userImage.src = `${userPhoto}`;
+    userName.innerHTML = `${user.displayName}`;
+    userProfile.src = `${userPhoto}`;
+    userDisplayName.innerHTML = `${user.displayName}`;
+  } else {
+    window.location = "login.html";
+  }
+});
+
+//Logout function
+
+// const popup= document.querySelector('#popup')
+
+logOutBtn.addEventListener("click", () => {
   signOut(auth)
     .then(() => {
-      alert('You are Log Out sucessfully!')
-      window.location="login.html"
+      alert("You are Logout Sucessfully");
+      window.location = "login.html";
     })
     .catch((error) => {
       console.log(error);
     });
-  
-  
-  })
+});
+
+//Navbar functionality
+
+// JavaScript code for handling modal functionality
+document.addEventListener("DOMContentLoaded", () => {
+  const userProfile = document.getElementById("userProfile");
+  const userModal = document.getElementById("userModal");
+  const closeBtn = document.getElementById("closeBtn");
+
+  userProfile.addEventListener("click", () => {
+    userModal.classList.add("show");
+    document.querySelector(".modal-content").classList.add("show");
+  });
+
+  closeBtn.addEventListener("click", () => {
+    userModal.classList.remove("show");
+    document.querySelector(".modal-content").classList.remove("show");
+  });
+
+  userModal.addEventListener("click", (e) => {
+    if (e.target === userModal) {
+      userModal.classList.remove("show");
+      document.querySelector(".modal-content").classList.remove("show");
+    }
+  });
+});
+
+//Todo app script started
+
+let form = document.querySelector("#form");
+let todo = document.querySelector("#todo");
+let submitBtn = document.querySelector("#btn-submit");
+let ul = document.querySelector("#ul");
+
+let arr = [];
+
+async function getData() {
+  const querySnapshot = await getDocs(collection(db, "todo"));
+  querySnapshot.forEach((doc) => {
+    arr.push(doc.data());
+    console.log(doc.data());
+  });
+
+  render();
+}
+
+getData();
+
+function render() {
+  if (arr.length === 0) {
+    ul.innerHTML = `<p class="mt-6 no-item">No Todo Found....</p>`;
+    return;
+  }
+
+  ul.innerHTML = "";
+
+  arr.map((item) => {
+    ul.innerHTML += `<li class="todo-item mt-6 ">
+            <span class="todo-text">${item.todo}</span>
+            <div class="todo-actions">
+                <button class="todo-edit-btn"><i class="fas fa-edit"></i> Edit</button>
+                <button class="todo-delete-btn"><i class="fas fa-trash"></i> Delete</button>
+            </div>
+        </li>`;
+  });
+}
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  arr.push({
+    todo: todo.value,
+  });
+
+  render();
+
+  try {
+    const docRef = await addDoc(collection(db, "todo"), {
+      todo: todo.value,
+    });
+
+    todo.value = "";
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+});
